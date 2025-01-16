7ac698c756e59e5d63ee870637a4b63c4ec0a9c1
SurajMahar$ <92042296+surajmahar@users.noreply.github.com>
2025-01-16 20:50:51 +0530

    Update CoursePage.jsx

--- user/src/components/CoursePage.jsx
+++ user/src/components/CoursePage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
-import Appbar from './Appbar.jsx';
+import Appbar from './AppBar.jsx';
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
